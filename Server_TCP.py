1841e5b46ff7db50621e7dc8772a7333bce8de86
Michael Booth <michaelbooth08@gmail.com>
2018-11-16 15:26:16 -0500

    Changed comments to PEP8

--- Server_TCP.py
+++ Server_TCP.py
@@ -1,44 +1,54 @@
 import socket
 from datetime import datetime
 
-TCP_IP = '192.168.50.158'           # IP address to connect to
-TCP_PORT = 5005                     # Port to connect to
-BUFFER_SIZE = 1024                  # Buffer size for receiving message
-connection = True                   # Flag to determine if to exit or end
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # Establish socket
-s.bind((TCP_IP, TCP_PORT))                                  # Bind to client IP & port
-
-while True:                                                 # Loop until connection ended
-    s.listen(1)                                             # Listen for connection
-    conn, addr = s.accept()                                 # Establish connection
-    print("Connection to Client Established")
-    print('Server Address:', TCP_IP)
-    print('Client Address:', addr[0])
+TCP_IP = 'X.X.X.X'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+connection = True
 
 
-    while connection:                                       # Loop until end or exit
-        data = conn.recv(BUFFER_SIZE)                       # Receive message from client
-        if (data.decode("utf-8") == "What is the current date and time?"):          # Decode response and reply
-            time = datetime.now()                                                   # Get current date and time
-            message = "Current Date and Time - " + time.strftime("%m/%d/%Y %H:%M:%S")
-            conn.sendall(message.encode('ascii'))                                   # Encode message and send
-        elif (data.decode("utf-8") == "Exit"):
-            conn.sendall(b'')                                                       # If exit, send back empty response
-            connection = False                                                      # Set flag to false then break
+def rcvPacket():
+    """
+    Receive messages over TCP and respond based on the message
+
+    Send back date and time when requested, else end or exit the session
+    """""
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+
+    while True:
+        s.listen(1)
+        conn, addr = s.accept()
+        print("Connection to Client Established")
+        print('Server Address:', TCP_IP)
+        print('Client Address:', addr[0])
+
+        while connection:
+            data = conn.recv(BUFFER_SIZE)
+            if (data.decode("utf-8") == "What is the current date and time?"):
+                time = datetime.now()
+                message = "Current Date and Time - " + time.strftime("%m/%d/%Y %H:%M:%S")
+                conn.sendall(message.encode('ascii'))
+            elif (data.decode("utf-8") == "Exit"):
+                conn.sendall(b'')
+                connection = False
+                break
+            elif (data.decode("utf-8") == "End"):
+                conn.sendall(b'')
+                break
+            else:
+                conn.sendall(b"Error: invalid request")
+
+        if not connection:
             break
-        elif (data.decode("utf-8") == "End"):
-            conn.sendall(b'')                                                       # If end, send back empty response
-            break                                                                   # then break
-        else:
-            conn.sendall(b"Error: invalid request")                                 # Reply with error message
-                                                                                    # for invalid request
-    if not connection:                              # Break if flag was set
-        break
-    print("Connection to Client Terminated")
-    print("Listening for new connection\n")
-    conn.close()                                    # Close connection and wait for new one
+        print("Connection to Client Terminated")
+        print("Listening for new connection\n")
+        conn.close()
+
+    conn.close()
+    print("Server terminated")
 
 
-conn.close()                                        # Close connection and end program
-print("Server terminated")
+def __main__(self):
+    rcvPacket()
