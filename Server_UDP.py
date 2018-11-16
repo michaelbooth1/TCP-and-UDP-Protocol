db6b48301cf76ee471c5fb20afa4e277ed7255ad
Michael Booth <michaelbooth08@gmail.com>
2018-11-16 15:26:45 -0500

    Changed comments to PEP8

--- Server_UDP.py
+++ Server_UDP.py
@@ -1,42 +1,50 @@
 import socket
 from datetime import datetime
 
-UDP_IP = '192.168.50.158'               # IP address to connect to
-UDP_PORT = 5005                         # Port to connect to
-BUFFER_SIZE = 1024                      # Buffer size for receiving message
-connection = True                       # Flag to determine if to exit or end
-
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)                # Establish socket
-    s.bind((UDP_IP, UDP_PORT))                                          # Connect to client
-    print("Server binded to ",UDP_IP,":",UDP_PORT)
-
-    while connection:                                                           # Loop until exit or end
-        data, addr = s.recvfrom(BUFFER_SIZE)                                    # Recive date and addr of sender
-
-        if (data.decode("utf-8") == "What is the current date and time?"):      # Decode message and respond
-            time = datetime.now()                                               # Get current date and time
-            message = "Current Date and Time - " + time.strftime("%m/%d/%Y %H:%M:%S")
-            s.sendto(message.encode('ascii'), addr)             # Encode and respond with date/time
-
-        elif (data.decode("utf-8") == "Exit"):
-            s.sendto(b'', addr)                                 # Send back empty response and close connection
-            connection = False
-            break
-
-        elif (data.decode("utf-8") == "End"):                   # Send back empty response and wait for new connection
-            s.sendto(b'', addr)
-            break
-
-        else:
-            s.sendto(b"Error invalid request", addr)            # Send error message if invalid request
-
-    if not connection:                                          # If connection flag wasn't set to false, then loop
-            break
-
-print("Server terminated")                                      # Close server
-s.close()
+UDP_IP = 'X.X.X.X'
+UDP_PORT = 5005
+BUFFER_SIZE = 1024
+connection = True
 
 
+def rcvPacket():
+    """
+    Receive messages over UDP and respond based on the message
+
+    Send back date and time when requested, else end or exit the session
+    """""
+
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind((UDP_IP, UDP_PORT))
+        print("Server binded to ", UDP_IP, ":", UDP_PORT)
+
+        while connection:
+            data, addr = s.recvfrom(BUFFER_SIZE)
+
+            if (data.decode("utf-8") == "What is the current date and time?"):
+                time = datetime.now()
+                message = "Current Date and Time - " + time.strftime("%m/%d/%Y %H:%M:%S")
+                s.sendto(message.encode('ascii'), addr)
+
+            elif (data.decode("utf-8") == "Exit"):
+                s.sendto(b'', addr)
+                connection = False
+                break
+
+            elif (data.decode("utf-8") == "End"):
+                s.sendto(b'', addr)
+                break
+
+            else:
+                s.sendto(b"Error invalid request", addr)
+
+        if not connection:
+                break
+
+    print("Server terminated")
+    s.close()
 
 
+def __main__(self):
+    rcvPacket()
