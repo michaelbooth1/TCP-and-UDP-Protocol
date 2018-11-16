a75c8fe46268d5a2850e41f8ae23666e78b4ce4d
Michael Booth <michaelbooth08@gmail.com>
2018-11-16 15:25:52 -0500

    Changed comments to PEP8

--- Client_UDP.py
+++ Client_UDP.py
@@ -1,18 +1,30 @@
 import socket
 
-UDP_IP = '192.168.50.158'               # IP address to connect to
-UDP_PORT = 5005                         # Port to connect to
-BUFFER_SIZE = 1024                      # Buffer size for receiving message
+UDP_IP = 'X.X.X.X'
+UDP_PORT = 5005
+BUFFER_SIZE = 1024
 
-print ("Attempting to contact server at ",UDP_IP,":",UDP_PORT)
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)                # Establish socket
 
-while True:
-    message = input("Enter command to send to server: ")            # Get message from user
-    s.sendto(str.encode(message), (UDP_IP, UDP_PORT))               # Encode message to bytes, send to IP & port
-    data, server = s.recvfrom(BUFFER_SIZE)                          # Receive response from server
-    if not data:                                                    # If no data back, end client
-        break
-    print(data.decode("ascii"))                                     # Print response
+def sendPacket():
+    """
+    Connect to socket and send message over UDP
 
-print("Client ended")
+    Exit when empty string is return from server
+    """""
+
+    print ("Attempting to contact server at ", UDP_IP, ":", UDP_PORT)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    while True:
+        message = input("Enter command to send to server: ")
+        s.sendto(str.encode(message), (UDP_IP, UDP_PORT))
+        data, server = s.recvfrom(BUFFER_SIZE)
+        if not data:
+            break
+        print(data.decode("ascii"))
+
+    print("Client ended")
+
+
+def __main__(self):
+    sendPacket()
